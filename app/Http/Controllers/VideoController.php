f74ce05cdf9fbb64cf574c61dc35917471514a19
maximiliian <johaneshamonangansilalahi@gmail.com>
2021-03-23 20:18:15 +0700

    add streaming function

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\MOdels\Video;
+
+class VideoController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+}
